cc807470a37e1888bba8e512851d0d34557eee8b
Daniel <dgkartin@live.dk>
2017-10-19 11:42:55 +0200

    Made it read data from webcam

--- CMakeFiles/opencvFilters.dir/DependInfo.cmake
+++ CMakeFiles/opencvFilters.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/daniel/CLionProjects/opencvFilters/main.cpp" "/home/daniel/CLionProjects/opencvFilters/CMakeFiles/opencvFilters.dir/main.cpp.o"
+  "/home/daniel/Documents/opencvFilters/main.cpp" "/home/daniel/Documents/opencvFilters/CMakeFiles/opencvFilters.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- main.cpp
+++ main.cpp
@@ -5,25 +5,29 @@ using namespace cv;
 
 int main() {
 
-    Mat img, edges;
-    img = imread("/home/daniel/CLionProjects/opencvFilters/Lenna.png", CV_LOAD_IMAGE_UNCHANGED);
-
-    if ( !img.data ) {
-        printf("No image data \n");
+    VideoCapture cap(0);
+    if (!cap.isOpened())
         return -1;
+
+    Mat img, edges;
+    //img = imread("/home/daniel/Documents/opencvFilters/Lenna.png", CV_LOAD_IMAGE_UNCHANGED);
+
+    for(;;) {
+        Mat frame;
+        cap >> frame;
+        cvtColor(frame, edges, CV_BGR2GRAY);
+
+        GaussianBlur(edges, edges, Size(5, 5), 1.5, 1.5);
+        Canny(edges, edges, 0, 60, 3);
+
+        imshow("Edges", edges);
+
+        //namedWindow("Lenna", WINDOW_AUTOSIZE);
+        //imshow("Lenna", img);
+
+        if(waitKey(30) >= 0)
+            break;
     }
-
-    cvtColor(img, edges, CV_BGR2GRAY);
-
-    GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-    Canny(edges, edges, 0, 30, 3);
-
-    imshow("Edges", edges);
-
-    namedWindow("Lenna", WINDOW_AUTOSIZE);
-    imshow("Lenna", img);
-
-    waitKey(0);
 
     return 0;
 }
